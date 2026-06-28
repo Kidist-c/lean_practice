d3476defa6d3d402092d890db44ed7f40d01da43
kidistMulualem <mulualemkidist4@gmail.com>
2026-06-29 01:09:22 +0900

    prove the voltage

--- MyMathProject/equalities.lean
+++ MyMathProject/equalities.lean
@@ -18,3 +18,19 @@ example (x y :ℝ )
 x=((x + y) + (x - y))/2 := by ring --algebric rearrangement
 _ = (150 + 90) /2 := by rw[h1,h2]--- for substitution
 _=120 :=by ring
+
+
+/-
+ R=4 ohms
+ I=3 amp
+ prove that the voltage across the resistor 12 volts.
+ -/
+example (V I R:ℝ)
+(h1:R=4)
+(h2:I=3)
+(h3:V=I * R) :
+V=12:= calc
+V=I * R :=by rw[h3]--substitution
+_=3 * R :=by rw[h2]--substitution
+_=3 * 4 :=by rw[h1]--substitution
+_=12 :=by norm_num--Numerical Simplification
